24c34fb8b128ca62347c44dc5408a3fe8c06be88
MynxGithub <35780058+MynxGithub@users.noreply.github.com>
2018-01-29 10:48:30 +0000

    Update main.clj

--- main.clj
+++ main.clj
@@ -1,6 +1,7 @@
                           ;;AI Programming 
    ;;-----------------------------------------------------------
-  ;; 1. Using only the first and rest functions, write the sequences of expressions that retrieve the X from the following lists/vectors. -----------------------------------------------------------
+  ;; 1. Using only the first and rest functions, write the sequences of expressions that retrieve the X from the following lists/vectors.
+   ;;-----------------------------------------------------------
 
 ;; 1.1: ‘(a X b c)
 (first (rest '(a X b c)))
@@ -22,7 +23,8 @@
 
 
   ;;-----------------------------------------------------------
-  ;; 2. Using only the following symbols: cons 'a 'b 'c () Write the expressions which build the following lists: -----------------------------------------------------------
+    ;; 2. Using only the following symbols: cons 'a 'b 'c () Write the expressions which build the following lists: 
+  ;;-----------------------------------------------------------
 
 ;; 2.1: (a b c)
 (cons 'a (cons 'b(cons 'c())))
@@ -34,7 +36,9 @@
 (cons (cons 'a())(cons 'b(cons 'c())))
 
   ;;-----------------------------------------------------------
-  ;; 3. count is a function that takes a list (or vector or ...) as its argument and returns as its value the length of that list. So count applied to '(a b c d) gives a value of 4. Look at the lists in problem-2 and 1.5 & 1.6 in problem-1, noting what you think their lengths should be, then check using count. If you find the answer is not what you had expected make sure you know why. -----------------------------------------------------------
+    ;; 3. count is a function that takes a list (or vector or ...) as its argument and returns as its value the length of that list. So count applied to '(a b c d) gives a value of 4. Look at the lists in problem-2 and 1.5 & 1.6 in problem-1, noting what you think their lengths should be, then check using count. If you find the answer is not what you had expected make sure you know why. 
+  ;;-----------------------------------------------------------
+
 ;; 1.5
 (count '(a (b (X (c)))))
 ;; length = 2.
@@ -56,7 +60,8 @@
 ;; length = 3.
 
   ;;-----------------------------------------------------------
-  ;; 4. Write the following functions -----------------------------------------------------------
+    ;; 4. Write the following functions 
+  ;;-----------------------------------------------------------
 
 ;; 4.1  inc-num takes 1 arg which is a number & returns the number incremented by 1 – keep it simple.
 (defn inc-num [n] (inc n))
@@ -71,7 +76,8 @@
 (vector-head '(a b c))
 
 ;;-----------------------------------------------------------
-;; 5. find the most appropriate way to retrieve 'spam' from the following data structures... -----------------------------------------------------------
+  ;; 5. find the most appropriate way to retrieve 'spam' from the following data structures... 
+;;-----------------------------------------------------------
 
 ;; 5.1
 ('{:a egg, :b spam, :c chips} :b)
@@ -85,10 +91,11 @@
 
 
 ;; 5.4: investigate at 5.3 & describe all the data structures it contains.
-;; Question 5.3 has Lists, Vectors, maps and a set 
+Question 5.3 has Lists, Vectors, maps and a set 
 
 ;;-----------------------------------------------------------
-;; 6. There is a number puzzle where you start from one number and have to reach another number in as few steps as possible using only a small number of mathematical operations. For example: given legal mathematical operations of multiplying by 10, dividing by 2, adding 5 and subtracting 3, what step are required to get from 7 to 57. -----------------------------------------------------------
+  ;; 6. There is a number puzzle where you start from one number and have to reach another number in as few steps as possible using only a small number of mathematical operations. For example: given legal mathematical operations of multiplying by 10, dividing by 2, adding 5 and subtracting 3, what step are required to get from 7 to 57. 
+;;-----------------------------------------------------------
 
 ;; breadth first search mechanism
 ;; @args start start state
@@ -143,5 +150,6 @@
 
 
 ;;-----------------------------------------------------------
-;; 7. Design a state representation and a transformation function for the “Farmer, Dog, Goose, Corn” problem. -----------------------------------------------------------
+  ;; 7. Design a state representation and a transformation function for the “Farmer, Dog, Goose, Corn” problem. 
+;;-----------------------------------------------------------
 (defn initial-state #{:Farmer, :Dog, :Goose, :Corn} #{:boat} #{})
